cc150c3bacc884a53c095461fc772de32bed8333
AK-Khan02 <83300832+AK-Khan02@users.noreply.github.com>
2024-01-27 18:56:58 -0500

    Create Reverse List.py

---
+++ Linked Lists/Reverse List.py
@@ -0,0 +1,10 @@
+def reverse(self):
+    """Reverse the linked list in-place."""
+    prev = None
+    current = self.head
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    self.head = prev
